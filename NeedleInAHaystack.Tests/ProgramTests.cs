1291992b6c0682a7f1256e4e2b51d53d4d3c04dd
Victor Johnsson <victor.johnsson@gmail.com>
2022-04-14 12:14:18 +0200

    added support for files without extensions

--- NeedleInAHaystack.Tests/ProgramTests.cs
+++ NeedleInAHaystack.Tests/ProgramTests.cs
@@ -10,14 +10,15 @@ public class ProgramTests
 {
 
     [Test]
-    [TestCase("resources/empty.txt", 0)]
-    [TestCase("resources/one-match.json", 1)]
-    [TestCase("resources/two-matches.xml", 2)]
+    [TestCase("empty.txt", 0)]
+    [TestCase("one-match.json", 1)]
+    [TestCase("one-match", 1)]
+    [TestCase("two-matches.xml", 2)]
     public void Should_write_how_many_times_the_base_filename_occurs_in_the_file(
         string inputFile,
         int expectedMatches)
     {
-        ConsoleOutputOf(() => Program.Main(new string[] { inputFile }))
+        ConsoleOutputOf(() => Program.Main(new string[] { $"resources/{inputFile}" }))
                 .Should().Be(SingleLine($"found {expectedMatches}"));
     }
 
--- NeedleInAHaystack/Program.cs
+++ NeedleInAHaystack/Program.cs
@@ -9,8 +9,7 @@ public class Program
 
     public static void Main(string[] args)
     {
-        string filename = Path.GetFileName(args[0]);
-        string baseFilename = filename.Substring(0, filename.IndexOf('.'));
+        string baseFilename = Path.GetFileNameWithoutExtension(args[0]);
         int nbrOfMatches = 0;
         foreach (string line in System.IO.File.ReadLines(args[0]))
             if (line.Contains(baseFilename))
